42059fb6dc4b2e8ce20ccff358f40bba52a9557e
itsc222 <ischneid87@gmail.com>
2023-07-19 09:03:47 -0700

    added aespa

--- ccl-download-Hangul-chatgpt.py
+++ ccl-download-Hangul-chatgpt.py
@@ -17,7 +17,7 @@ main_df = pl.DataFrame(main_df_data, schema = {'title': str,
 
 
 # Specify the URL of the webpage to scrape
-url = "https://colorcodedlyrics.com/2023/06/15/exo-let-me-in/"
+url = "https://colorcodedlyrics.com/2020/11/17/aespa-black-mamba/"
 
 # Send a GET request to the webpage
 response = requests.get(url)
